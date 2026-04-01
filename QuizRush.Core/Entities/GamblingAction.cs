a00b4db3588dd1a5a961e36068c2182c6b1c731e
Matej <mpilic1@algebra.hr>
2026-04-01 16:42:16 +0200

    Add GameSession, Player, PlayerAnswer and GamblingAction entities
    TODO: migrations need to be updated to reflect new entities
    Run: dotnet ef migrations add AddRuntimeEntities --project QuizRush.Infrastructure --startup-project QuizRush.Api
    Then: dotnet ef database update --project QuizRush.Infrastructure --startup-project QuizRush.Api

---
+++ QuizRush.Core/Entities/GamblingAction.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizRush.Core.Entities
+{
+    public class GamblingAction
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public long PlayerId { get; set; }
+        [Required]
+        public long GameSessionId { get; set; }
+
+        public int PointsGambled { get; set; }
+        public bool Won { get; set; }
+
+        public Player Player { get; set; } = null!;
+        public Question Question { get; set; } = null!;
+        public GameSession GameSession { get; set; } = null!;
+
+    }
+}
---
+++ QuizRush.Core/Entities/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizRush.Core.Entities
+{
+    public class Player
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public long GameSessionId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public int Score { get; set; } = 0;
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+
+        public long? UserId { get; set; }
+
+        public GameSession GameSession { get; set; } = null!;
+        public User? User { get; set; }
+    }
+}
